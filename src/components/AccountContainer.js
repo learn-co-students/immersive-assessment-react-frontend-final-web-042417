aaf50be2181c8aaca5428b62d3ff5e6d0000e4fa
sammygee140 <samuelzgoldsmith@gmail.com>
2017-07-14 10:34:32 -0400

    done

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -8,57 +8,43 @@ class AccountContainer extends Component {
     super()
 
     this.state = {
-      transactions: [
-        {
-          id: 1,
-          posted_at: "2017-02-28 11:00:00",
-          description: "Leather Pants, Gap co.",
-          category: "Fashion",
-          amount: -20000
-        },
-        {
-          id: 2,
-          posted_at: "2017-02-29 10:30:00",
-          description: "Paycheck from Bob's Burgers",
-          category: "Income",
-          amount: 100000
-        },
-        {
-          id: 3,
-          posted_at: "2017-05-24 10:53:00",
-          description: "'Pair Programming Illuminated' by Laurie Williams and Robert Kessler",
-          category: "Entertainment",
-          amount: -1498
-        },
-        {
-          id: 4,
-          posted_at: "2017-05-24 08:52:00",
-          description: "Medium Iced Cold Brew, Gregory's Coffee",
-          category: "Food",
-          amount: -365
-        }
-      ],
+      transactions: [],
       activeCategory: "All"
     }
   }
 
-  handleChange() {
-    //... your code here
+  componentWillMount() {
+    fetch('https://boiling-brook-94902.herokuapp.com/transactions')
+      .then(resp => resp.json())
+      .then(data => this.setState({
+        transactions: data
+      }))
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      activeCategory: event.target.value
+    })
+  }
+
+  selectButton() {
+    return this.state.activeCategory === "All" ? this.state.transactions : this.state.transactions.filter(transaction => transaction.category.includes(this.state.activeCategory))
   }
 
   render() {
-    const displayedTransactions = this.state.transactions
+    const displayedTransactions = this.selectButton()
 
     return (
       <div className="ui grid container">
 
         <CategorySelector
           activeCategory={ this.state.activeCategory }
-          handleChange={ "...your code here" }
+          handleChange={ this.handleChange }
         />
 
         <TransactionsList
           transactions={ displayedTransactions }
+
         />
 
       </div>
